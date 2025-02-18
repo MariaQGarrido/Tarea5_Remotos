f0a516999702a9f158c6a0a320fb7ba7c11f83bb
andree <54333719+andreedev@users.noreply.github.com>
2025-02-18 12:10:53 +0100

    creamos la clase cocina

---
+++ tarea/Cocina.java
@@ -0,0 +1,40 @@
+package tarea;
+
+/**
+ * @author Cristhian C. && María Q Garrido
+ */
+public class Cocina extends Casa{
+    protected boolean tendedero;
+    protected double metrosT;
+
+    /**
+     *
+     * @param nombre
+     * @param metrosC
+     * @param numPuertas
+     * @param numVentanas
+     * @param tendedero
+     * @param metrosT
+     */
+    public Cocina(String nombre, double metrosC, int numPuertas, int numVentanas, boolean tendedero, double metrosT) {
+        super(nombre, metrosC, numPuertas, numVentanas);
+        this.tendedero = tendedero;
+        this.metrosT = metrosT;
+    }
+
+    /**
+     *
+     * @return tendedero: valor booleano que indica si tiene tendedero o no
+     */
+    public boolean isTendedero() {
+        return tendedero;
+    }
+
+    /**
+     *
+     * @return metros
+     */
+    public double getMetrosT() {
+        return metrosT;
+    }
+}
