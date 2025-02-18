582a30fa07cce5370daa54000da47da75b5d4bec
andree <54333719+andreedev@users.noreply.github.com>
2025-02-18 12:24:48 +0100

    creamos la clase habitacion

---
+++ tarea/Habitacion.java
@@ -0,0 +1,40 @@
+package tarea;
+
+/**
+ * @author Cristhian C. && María Q Garrido
+ */
+public class Habitacion extends Casa{
+    protected int numPuertasArmario;
+    protected boolean banio;
+
+    /**
+     *
+     * @param nombre
+     * @param metrosC
+     * @param numPuertas
+     * @param numVentanas
+     * @param numPuertasArmario
+     * @param banio
+     */
+    public Habitacion(String nombre, double metrosC, int numPuertas, int numVentanas, int numPuertasArmario, boolean banio) {
+        super(nombre, metrosC, numPuertas, numVentanas);
+        this.numPuertasArmario = numPuertasArmario;
+        this.banio = banio;
+    }
+
+    /**
+     *
+     * @return numPuertasArmario: un valor entero que contiene el numero de puertas del armario empotrado
+     */
+    public int getNumPuertasArmario() {
+        return numPuertasArmario;
+    }
+
+    /**
+     *
+     * @return baño: valor booleano que indica si tiene acceso directo a un baño o no
+     */
+    public boolean isBanio() {
+        return banio;
+    }
+}
