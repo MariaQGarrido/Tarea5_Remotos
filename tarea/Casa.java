226f650c85f007bbdd16f686c81afe23a5602c39
MariaQGarrido <mariaqingxuan@gmail.com>
2025-02-18 11:48:37 +0100

    Aquí hemos creado la clase padre casa con sus atributos

---
+++ tarea/Casa.java
@@ -0,0 +1,60 @@
+package tarea;
+
+public class Casa {
+    //elegimos protected para que las sublcases puedan acceder a estos atributos.
+ protected String nombre;
+ protected double metrosC;
+ protected int numPuertas;
+ protected int numVentanas;
+
+    /**
+     *
+     * @param nombre
+     * @param metrosC
+     * @param numPuertas
+     * @param numVentanas
+     */
+    public Casa(String nombre, double metrosC, int numPuertas, int numVentanas) {
+        this.nombre = nombre;
+        this.metrosC = metrosC;
+        this.numPuertas = numPuertas;
+        this.numVentanas = numVentanas;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getMetrosC() {
+        return metrosC;
+    }
+
+    public void setMetrosC(double metrosC) {
+        this.metrosC = metrosC;
+    }
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public int getNumVentanas() {
+        return numVentanas;
+    }
+
+    public void setNumVentanas(int numVentanas) {
+        this.numVentanas = numVentanas;
+    }
+}
+
